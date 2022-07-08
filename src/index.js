e7661ccd29dd8d5eb23792f18e4edaee57d0a1db
Md. Sariful Islam <ssahed65@gmail.com>
2022-07-08 10:36:04 +0600

    initial

--- src/index.js
+++ src/index.js
@@ -6,11 +6,10 @@ import { BrowserRouter } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
 root.render(
-  <>
+  <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </>
+  </React.StrictMode>
 );
